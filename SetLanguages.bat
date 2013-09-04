76cc60c94c5811fcaefb8915f27387c74f46cc07
Jacob Boerema <jgboerema@gmail.com>
2013-09-05 00:27:44 +0200

    Add Italian and Swedish to translations update script. Finally noticed that these 2 languages were not updated.

--- SetLanguages.bat
+++ SetLanguages.bat
@@ -3,4 +3,4 @@
 :: This is the list of languages that are known to Transcript
 :: Update this line when a new language is added and all other batch files
 :: will benefit from the new value
-set LANGUAGES=de fr nl da fi
+set LANGUAGES=da de fi fr it nl sv
