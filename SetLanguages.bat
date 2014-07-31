2d907916ee026553c0be0ec57d4f52cfa0e37a3f
Jacob Boerema <jgboerema@gmail.com>
2014-07-31 17:31:38 +0200

    Add Spanish translation

--- SetLanguages.bat
+++ SetLanguages.bat
@@ -3,4 +3,4 @@
 :: This is the list of languages that are known to Transcript
 :: Update this line when a new language is added and all other batch files
 :: will benefit from the new value
-set LANGUAGES=da de fi fr it nl sv cs
+set LANGUAGES=da de es fi fr it nl sv cs
