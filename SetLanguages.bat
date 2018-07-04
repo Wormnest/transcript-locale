f02410e331356827d6cfb6c17e447a636d5b9698
Jacob Boerema <jgboerema@gmail.com>
2018-07-04 16:18:48 +0200

    Change: Replace Portuguese po with two Portuguese ones for PT and BR.

--- SetLanguages.bat
+++ SetLanguages.bat
@@ -3,4 +3,4 @@
 :: This is the list of languages that are known to Transcript
 :: Update this line when a new language is added and all other batch files
 :: will benefit from the new value
-set LANGUAGES=cs da de es fi fr it nl no pt sv
+set LANGUAGES=cs da de es fi fr it nl no pt_BR pt_PT sv
